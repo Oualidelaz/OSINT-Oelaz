a14920cdf5c0320c43040e476dd9f7741b9db7c7
Oualidelaz <gthb112000@gmail.com>
2026-08-01 23:41:30 +0000

    feat(cli): add terminal colors screen cleaning and application banner

---
+++ home/global_config.py
@@ -0,0 +1,50 @@
+import os
+
+class Colors:
+    def __init__(self) -> None:
+        self.BLACK = "\033[0;30m"
+        self.RED = "\033[0;31m"
+        self.GREEN = "\033[0;32m"
+        self.BROWN = "\033[0;33m"
+        self.BLUE = "\033[0;34m"
+        self.PURPLE = "\033[0;35m"
+        self.CYAN = "\033[0;36m"
+        self.LIGHT_GRAY = "\033[0;37m"
+        self.DARK_GRAY = "\033[1;30m"
+        self.LIGHT_RED = "\033[1;31m"
+        self.LIGHT_GREEN = "\033[1;32m"
+        self.YELLOW = "\033[1;33m"
+        self.LIGHT_BLUE = "\033[1;34m"
+        self.LIGHT_PURPLE = "\033[1;35m"
+        self.LIGHT_CYAN = "\033[1;36m"
+        self.LIGHT_WHITE = "\033[1;37m"
+        self.BOLD = "\033[1m"
+        self.FAINT = "\033[2m"
+        self.ITALIC = "\033[3m"
+        self.UNDERLINE = "\033[4m"
+        self.BLINK = "\033[5m"
+        self.NEGATIVE = "\033[7m"
+        self.CROSSED = "\033[9m"
+        self.END = "\033[0m"
+
+
+def clean():
+    os.system("cls") if os.name == "nt" else os.system("clear")
+
+def home():
+    colors = Colors()
+
+    print(f"""{colors.RED}
+              .__                     ________    _________.___ __________________
+  ____   ____ |  | _____  ________    \_____  \  /   _____/|   |\      \__    ___/
+ /  _ \_/ __ \|  | \__  \ \___   /     /   |   \ \_____  \ |   |/   |   \|    |   
+(  <_> )  ___/|  |__/ __ \_/    /     /    |    \/        \|   /    |    \    |   
+ \____/ \___  >____(____  /_____ \    \_______  /_______  /|___\____|__  /____|   
+            \/          \/      \/            \/        \/             \/             
+{colors.END}
+                          [+] created.by.oelaz [+]
+{colors.GREEN}                                                                                               
+  ______   ______   ______   ______   ______   ______   ______   ______   ______    
+
+                                                                                                    
+{colors.END}""")
