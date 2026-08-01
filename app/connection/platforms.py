3a520ef1525c40b280e47d94a0634b9f9e0e3e0c
Oualidelaz <gthb112000@gmail.com>
2026-08-01 22:24:00 +0000

    data(username-tracker): add supported platform URLs and check strategies

---
+++ app/connection/platforms.py
@@ -0,0 +1,107 @@
+PLATFORMS = [
+    {
+        "name": "Instagram",
+        "url": "https://www.instagram.com/{username}/",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Facebook",
+        "url": "https://www.facebook.com/{username}",
+        "check_type": "content_or_redirect",
+    },
+    {
+        "name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/{username}/",
+        "check_type": "status_code",
+    },
+    {
+        "name": "X",
+        "url": "https://x.com/{username}",
+        "check_type": "content",
+    },
+    {
+        "name": "TikTok",
+        "url": "https://www.tiktok.com/@{username}",
+        "check_type": "content",
+    },
+    {
+        "name": "YouTube",
+        "url": "https://www.youtube.com/@{username}",
+        "check_type": "status_code",
+    },
+    {
+        "name": "Reddit",
+        "url": "https://www.reddit.com/user/{username}/",
+        "check_type": "content",
+    },
+    {
+        "name": "Pinterest",
+        "url": "https://www.pinterest.com/{username}/",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Twitch",
+        "url": "https://www.twitch.tv/{username}",
+        "check_type": "content",
+    },
+    {
+        "name": "Snapchat",
+        "url": "https://www.snapchat.com/add/{username}",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Telegram",
+        "url": "https://t.me/{username}",
+        "check_type": "content",
+    },
+    {
+        "name": "GitHub",
+        "url": "https://github.com/{username}",
+        "check_type": "status_code",
+    },
+    {
+        "name": "GitLab",
+        "url": "https://gitlab.com/{username}",
+        "check_type": "status_code",
+    },
+    {
+        "name": "Medium",
+        "url": "https://medium.com/@{username}",
+        "check_type": "content",
+    },
+    {
+        "name": "Tumblr",
+        "url": "https://{username}.tumblr.com/",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "SoundCloud",
+        "url": "https://soundcloud.com/{username}",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Vimeo",
+        "url": "https://vimeo.com/{username}",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Behance",
+        "url": "https://www.behance.net/{username}",
+        "check_type": "status_code",
+    },
+    {
+        "name": "Dribbble",
+        "url": "https://dribbble.com/{username}",
+        "check_type": "content_or_status",
+    },
+    {
+        "name": "Steam",
+        "url": "https://steamcommunity.com/id/{username}/",
+        "check_type": "content",
+    },
+    {
+        "name": "Linktree",
+        "url": "https://linktr.ee/{username}",
+        "check_type": "content",
+    },
+]
